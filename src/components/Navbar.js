9e81cbd7c53dd45308bea32a1bbc580a07f228c3
Furkan Doğan <1furkandogan1@gmail.com>
2023-08-07 23:00:32 +0300

    favorites added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,33 +68,14 @@ export default function Navbar() {
         <div id="otherProjects">
           <h2>My Social Accounts</h2>
           <a
-            href="https://www.github.com/doganfurkan"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
             github
           </a>
-          <a
-            href="https://www.instagram.com/1furkandogan1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            instagram
-          </a>
-          <a
-            href="https://www.linkedin.com/in/furkan-doğan"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            linkedin
-          </a>
-          <a
-            href="https://www.frontendmentor.io/profile/doganfurkan"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            frontendmentor
-          </a>
+          
+          
         </div>
       </div>
     </aside>
