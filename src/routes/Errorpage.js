cafc18182eee77a90c35132495e30cc5891cc2a8
Furkan Doğan <1furkandogan1@gmail.com>
2024-02-05 13:21:29 +0300

    stylize error page

--- src/routes/Errorpage.js
+++ src/routes/Errorpage.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Errorpage() {
   return (
-    <div>Errorpage</div>
+    <div style={{textAlign:"center", padding:"2rem"}}>There has been an error. Click down below to return back to the homepage. <br /> <br /> <Link to={"/"}>Go Back to Home</Link></div>
   )
 }
